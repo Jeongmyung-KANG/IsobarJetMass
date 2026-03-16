2b6bac9facb6c64cb246d9baa14480a6e6630c09
Jeongmyung_KANG <jeongmyung.kang@gmail.com>
2026-03-16 16:20:16 +0900

    update -- class directory reading via std stream

--- IsobarMacro/Unfolding/DataScanning.C
+++ IsobarMacro/Unfolding/DataScanning.C
@@ -1,10 +1,15 @@
+#define LOCALTEST
+#include <filesystem>
 TFile *fin;
 TFile *fout; 
 TTree *MB_tree; 
 TTree *outtree;
 TString MB_name; 
 
-void init(int fileIndex){ 
+namespace fs = std::filesystem;
+
+void init(int fileIndex){  
+
   MB_name = Form("/Users/gangjeongmyeong/Star/IsobarMacro/Unfolding/461/METracks_%d.root", fileIndex); 
   cout << MB_name << endl;
   fin =  TFile::Open(MB_name);   
@@ -21,6 +26,16 @@ void finish(){
  
 void DataScanning(){  
 
+#ifdef LOCALTEST
+    cout << "--------------WARNING-------------------" << endl;
+    cout << "-                                       " << endl; 
+    cout << "-       this is fucking local test please change your fucking status                                " << endl;  
+    cout << "-                                       " << endl; 
+    cout << "-                                       " << endl; 
+    cout << "--------------WARNING-------------------" << endl;
+#endif
+
+
   int inputIndex = 1; 
   int cumEventNumber = 0;  
   int eventIndex; 
@@ -32,22 +47,29 @@ void DataScanning(){
   outtree->Branch("eventIndex", &eventIndex);
   outtree->Branch("fileIndex", &fileIndex); 
 
-  for (int i = 0; i < 10000; i++){ 
-    init(i);   
+  
+  for (int i = 0; i < 640; i++){ 
     
-    cout << MB_tree->GetEntries() << " " << cumEventNumber <<endl;
+    string path; 
+    #ifdef LOCALTEST 
+    path = "/Users/gangjeongmyeong/Star/IsobarMacro/Unfolding/"; 
+    #endif
+
+    TString dir = Form("%d", i);
+    path += dir.Data();  
     
-    for (int e = 0; e < MB_tree->GetEntries(); e++){   
-      cout << MB_name << " " << i << " " << cumEventNumber << " " << e << endl; 
-      eventIndex = e;
-      fileIndex = i;
-      cumEventNumber ++; 
-      outtree->Fill(); 
+    if (!fs::exists(path)) {
+      cout << path << " " << "dir deos not exist" << endl;
+      continue;
     }
 
-    fin->Close(); 
+    for (const auto& entry : fs::directory_iterator(path)){  
+      TString MB_rootFileName = TString(entry.path());  
+      
+    }
+
   }
-  
+
   finish(); 
 
  }
